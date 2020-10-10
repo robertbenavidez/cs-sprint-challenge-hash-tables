2198d7094561e5729c39009d6a637efa0e758a93
Robert Benavidez <murabitbenavidez@gmail.com>
2020-10-10 15:19:36 -0400

    ex4 solution

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,15 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
+    result = []
+
+    for number in a:
+        # start cache with 1 because 0 is neither negative nor positive
+        cache[number] = 1
+        if number != 0 and -number in cache:
+            # abs will normalize appending only positive numbers
+            result.append(abs(number))
 
     return result
 
