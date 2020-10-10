737e45f249a3a1d8e16faa1669153cc0e5567463
Robert Benavidez <murabitbenavidez@gmail.com>
2020-10-10 14:36:08 -0400

    ex1 solution

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,13 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
+    for i, weight in enumerate(weights):
+        try:
+            second_i = cache[limit - weight]
+        except:
+            cache[weight] = i
+        else:
+            return tuple([i, second_i])
 
     return None
